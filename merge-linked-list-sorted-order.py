dc2c5e70a0fc89c60c6549de9454217b00c3056b
Avinash Dalvi <avinashsdalvi@gmail.com>
2022-06-20 16:18:01 +0530

    Create merge-linked-list-sorted-order.py

---
+++ merge-linked-list-sorted-order.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        cur = dummy = ListNode()
+        while list1 and list2: 
+            if list1.val < list2.val:
+                cur.next = list1
+                cur = cur.next
+                list1 = list1.next
+            else:
+                cur.next = list2
+                cur = cur.next
+                list2 = list2.next
+            
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+
+        return dummy.next
