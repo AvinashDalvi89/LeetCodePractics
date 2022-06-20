f430c05fd0f79550d6bc0d00646e01fb37f2163f
Avinash Dalvi <avinashsdalvi@gmail.com>
2022-06-20 18:49:36 +0530

    Update rotate-linked-list-k-times.py

--- rotate-linked-list-k-times.py
+++ rotate-linked-list-k-times.py
@@ -1,3 +1,11 @@
+# Logic here is 
+# 5 element if 2 times rotates then rotation will 2 times
+# 3 element if 4 times rotates then answer will 1nd rotation means 1 times 
+# 3 element if 5 times rotates then answer will 2nd rotation means 2 times 
+# 3 element if 6 times rotates then answer will 0 rotation means directly return
+# 3 element if 2 times rotates then answer will 2nd rotation means 2 times 
+# first finding last node of list then pointing head to next of last node then finding module value rotation % size of list. Rotate for loop by size -k 
+# assign last to next of last. after for loop end assign current node to next of last and next of last become null or None 
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
