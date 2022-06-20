8cc1338de0cbeddc50ef2023db4b33501c56eb80
Avinash Dalvi <avinashsdalvi@gmail.com>
2022-06-20 22:55:42 +0530

    Create partition-list-by-k-number-linked-list.py

---
+++ partition-list-by-k-number-linked-list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        before = before_head = ListNode(0,head)
+        after = after_head = ListNode(0,head)
+        while head: 
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+        after.next = None
+        before.next = after_head.next
+        return before_head.next
