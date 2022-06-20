78cca286d014693c722d0caf015351450ac95206
Avinash Dalvi <avinashsdalvi@gmail.com>
2022-06-20 16:51:49 +0530

    Create swap-adjcent-node-without-change-values-linked-list.py

---
+++ swap-adjcent-node-without-change-values-linked-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(None, head)
+        prev, cur = dummy, head
+        while cur and cur.next:
+            prev.next = cur.next
+            cur.next = cur.next.next
+            prev.next.next = cur
+            prev, cur = cur, cur.next
+        return dummy.next
+        
