b20a60a076c64708e330da173268bce854d600d7
Avinash Dalvi <avinashsdalvi@gmail.com>
2022-06-21 17:19:59 +0530

    Create find-position-of-node-detect-cycle-linked-list.py

---
+++ find-position-of-node-detect-cycle-linked-list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return None
+        slow = fast = head
+        while fast and fast.next :
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:       
+                fast = head;
+                while slow != fast :
+                    slow = slow.next;
+                    fast = fast.next;
+                return slow
+        return None
